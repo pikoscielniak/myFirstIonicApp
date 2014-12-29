ecbc0c06a9c30059678924083e8bd78fe9b2ccdd
piotrekbass <piotrek.koscielniak@gmail.com>
2014-12-29 13:47:15 +0100

    ionic list

---
+++ www/app/home/leagues-ctrl.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular.module('eliteApp').controller('LeaguesCtrl', ['$state', 'eliteApi', LeaguesCtrl]);
+
+    function LeaguesCtrl($state, eliteApi) {
+        var vm = this;
+
+        var data = eliteApi.getLeagues();
+
+        vm.leagues = data;
+
+        vm.selectLeague = function (id) {
+            $state.go("app.teams");
+        };
+
+    };
+})();
